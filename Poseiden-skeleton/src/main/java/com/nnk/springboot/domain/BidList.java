d9a08a007b8e2fcf69336aa3592871ada29fc86b
Julien GUTTER <julien.gutter@gmail.com>
2024-09-30 11:27:46 +0200

    Maj modèle pour validation et lien SQL

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
@@ -4,10 +4,8 @@ package com.nnk.springboot.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
-import java.sql.Date;
 import java.sql.Timestamp;
 
 import org.hibernate.annotations.DynamicUpdate;
@@ -16,52 +14,78 @@ import org.hibernate.annotations.DynamicUpdate;
 @Data
 @DynamicUpdate
 @Entity
-@Table(name = "bidlist")
+@Table(name = "BidList")
 public class BidList {
     // TODO: Map columns in data table BIDLIST with corresponding java fields
 	@Id
+	@Column(name ="BidListId")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Integer BidListId;
 	
 	@Column
 	String account;
+
 	@Column
 	String type;
+
 	@Column
+	@Digits(integer=10,fraction=0)
 	Double bidQuantity;
+
 	@Column
+	@Digits(integer=10,fraction=0)
 	Double askQuantity;
+
 	@Column
+	@Digits(integer=10,fraction=0)
 	Double bid;
+
 	@Column
+	@Digits(integer=10,fraction=0)
 	Double ask;
+
 	@Column
 	String benchmark;
+
 	@Column
 	Timestamp bidListDate;
+
 	@Column
 	String commentary;
+
 	@Column
 	String security;
+
 	@Column
 	String status;
+
 	@Column
 	String trader;
+
 	@Column
 	String book;
+
 	@Column
 	String creationName;
+
 	@Column
 	Timestamp creationDate;
+
 	@Column
 	String revisionName;
+
 	@Column
 	Timestamp revisionDate;
+
 	@Column
 	String dealName;
+
 	@Column
 	String dealType;
+
 	@Column
 	String sourceListId;
+
 	@Column
 	String side;
 }
